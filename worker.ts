71c2cb607887557c0082853e5600f5e8137baae0
Ayushkhatidev <your-email@example.com>
2026-04-19 10:42:33 +0530

    fix: change static import to dynamic import in worker.ts

--- worker.ts
+++ worker.ts
@@ -1,8 +1,6 @@
-// @ts-ignore
-import server from "./dist/client/server/server.js";
-
 export default {
-  fetch: (request: Request, env: any, ctx: any) => {
-    return server.fetch(request, env, ctx);
+  async fetch(request: Request, env: any, ctx: any) {
+    const server = await import("./dist/client/server/server.js");
+    return server.default.fetch(request, env, ctx);
   },
 };
